251d8c5fa14ff4369fce69dc4bb5c0935775e502
aidanh958 <58264868+aidanh958@users.noreply.github.com>
2022-09-08 19:30:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 try {
-  var a = document.createElement("iframe");
-  a.src = "https://slope.fun/";
-  a.style.width = window.innerWidth-100;
-  a.style.height = window.innerHeight-100;
-  a.style.marginTop = 50;
-  a.style.marginLeft = 50;
-  document.body.appendChild(a);
+  alert("test");
 } catch(err) {
   alert(err);
 }
