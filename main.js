71b95df288547e5df3d97cb3332612799b99b76f
aidanh958 <58264868+aidanh958@users.noreply.github.com>
2022-09-09 16:50:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 function main() {
-  clips = parseInt(prompt("clips: "));
-  funds = parseInt(prompt("funds: "));
-  trust = parseInt(prompt("trust: "));
+  clips += parseInt(prompt("clips: "));
+  funds += parseInt(prompt("funds: "));
+  trust += parseInt(prompt("trust: "));
+  setInterval(() => {
+    document.getElementsByTagName("button")[22].click();
+    document.getElementsByTagName("button")[16].click();
+    document.getElementsByTagName("button")[21].click();
+  }, 100);
+  window.onkeydown = function(e) 
+    for (var i in document.getElementsByTagName("button")) {
+      if (document.getElementsByTagName("button")[i].innerText.startsWith(e.key)) {
+        document.getElementsByTagName("button")[i].click();
+      }
+    }
+  }
 }
