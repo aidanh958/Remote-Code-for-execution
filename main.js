53df463896aaa39746881cf231a6f4b289ba5584
aidanh958 <58264868+aidanh958@users.noreply.github.com>
2022-09-15 14:32:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,18 @@ function main() {
   trust += parseInt(prompt("trust: "));
   unsoldClips += parseInt(prompt("unsold clips: "));
   var done = false;
-  window.buttons = ["Make Paperclip"];
+  buttons = ["Make Paperclip"];
   setInterval(() => {
     document.getElementsByTagName("button")[22].click();
     document.getElementsByTagName("button")[16].click();
     document.getElementsByTagName("button")[21].click();
+    for (var e in window.buttons) {
+      for (var i in document.getElementsByTagName("button")) {
+        if (document.getElementsByTagName("button")[i].innerText.startsWith(buttons[e])) {
+          document.getElementsByTagName("button")[i].click();
+        }
+      }
+    }
     if (creativity < 0) {
       if (creativity < -100000000) {
         creativity = Math.abs(creativity);
@@ -16,17 +23,11 @@ function main() {
         creativity = 100000000;
       }
     };
-    for (var e in window.buttons) {
-      for (var i in document.getElementsByTagName("button")) {
-        if (document.getElementsByTagName("button")[i].innerText.startsWith(window.buttons[e])) {
-          document.getElementsByTagName("button")[i].click();
-        }
-      }
-    }
+    
   }, 100);
   window.onkeydown = function(e) {
     if (e.key === "a") {
-      window.buttons.push(prompt("press button: "));
+      buttons.push(prompt("press button: "));
     }
   };
 }
